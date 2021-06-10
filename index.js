da2836ef8980548cc03e20dc551118310547ce61
Anitson T T <anitsonttsat@gmail.com>
2021-06-10 12:11:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+<script>
+			  body {
+				background: yellow;
+				margin-top: 20px;
+				font-family: arial;
+				color: white;
+			  }
+			  .thumbnails img{
+				height: 80px;
+				border: 4px solid #555;
+				margin: 0 10px 10px 0;
+				padding: 1px;
+			  }
+			  
+			  .preview img{
+				border: 4px solid #444;
+				padding: 1px;
+				width: 800px;
+			  }
+</script>
